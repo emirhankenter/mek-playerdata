dfd1b9db83ee0b6a25f6fa3a2baa64ee3486142a
Emirhan Kenter <emirhan.kenter@rollicgames.com>
2022-11-28 19:55:15 +0300

    PlayerDataUsage Example

---
+++ Example/PlayerDataUsageExample.cs
@@ -0,0 +1,42 @@
+﻿using Mek.Models;
+using UnityEngine;
+
+namespace Mek.Examples
+{
+    public class PlayerDataUsageExample : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ExamplePlayerData.Initialize();
+
+            PrefsManager.Prefs[ExamplePrefKeys.ExampleInteger].Changed += OnExampleIntegerChanged;
+            PrefsManager.Prefs[ExamplePrefKeys.ExampleClass].Changed += OnExampleClassChanged;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                ExamplePlayerData.ExampleInteger++;
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                var exampleObject = ExamplePlayerData.ExampleClass;
+                exampleObject.Number++;
+                ExamplePlayerData.ExampleClass = exampleObject;
+            }
+        }
+
+        private void OnExampleIntegerChanged()
+        {
+            Debug.Log($"ExampleInteger Value changed = {ExamplePlayerData.ExampleInteger}");
+        }
+        
+        private void OnExampleClassChanged()
+        {
+            Debug.Log($"ExampleClassChanged Value changed = {ExamplePlayerData.ExampleClass.Number}");
+        }
+
+    }
+}
